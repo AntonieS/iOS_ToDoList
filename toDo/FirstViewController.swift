b36055e63ff17e1ca490930e92050c600df33133
antonie <antonies9@hotmail.com>
2019-03-13 16:47:18 +0200

    First commit

--- toDo/FirstViewController.swift
+++ toDo/FirstViewController.swift
@@ -7,11 +7,62 @@
 //
 
 import UIKit
+var list = [String]()
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var noItemsAdded: UILabel!
+    
+    @IBOutlet weak var myTabeView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            //Set the list again, so it refreshes without the deleted item.
+            UserDefaults.standard.set(list, forKey: "list")
+            checkEmptyArray()
+            myTabeView.reloadData()
+        }
+    }
+    
+    func loadDefaults() {
+        list = UserDefaults.standard.array(forKey: "list") as? [String] ?? []
+    }
+    
+    func checkEmptyArray() {
+        
+        if(list.isEmpty) {
+            noItemsAdded.text = "No Items added to your list yet"
+            print(list)
+        }
+        else {
+            noItemsAdded.text = ""
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkEmptyArray()
+        myTabeView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDefaults()
+        checkEmptyArray()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- toDo/SecondViewController.swift
+++ toDo/SecondViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: UIButton) {
+        
+            if (input.text != "")
+            {
+                let item: String = input.text!
+                print("I'm holding \(item)")
+                list.append(item)
+                
+                //Store item to array
+                UserDefaults.standard.set(input.text, forKey: "list")
+                UserDefaults.standard.synchronize()
+                input.text = ""
+                UserDefaults.standard.set(list, forKey: "list")
+            }
+            else {
+                
+                let alert = UIAlertController(title: "Whoops", message: "Ensure you have typed an item before clicking the add to list button.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true)
+        }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print(UserDefaults.standard.dictionaryRepresentation())
+        
+//        let AddedTasks = UserDefaults.standard.object(forKey: "AddedTask")
+//        if let AddedTask = AddedTasks as? String {
+//         input.text = AddedTask
+//         list.append(input.text!)
+        }
     }
-
-
-}
-
